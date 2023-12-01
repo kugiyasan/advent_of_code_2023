b96f75437edb9bb400723d2807823eb0df269258
kugiyasan <kugiyasan@users.noreply.github.com>
2023-12-01 17:20:13 -0500

    get_input.sh creates a new python boilerplate project

--- get_input.sh
+++ get_input.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -eo pipefail
 
@@ -8,14 +8,15 @@ DAY=$1
 NAME=$2
 FOLDER="d$1_$2"
 
-# Append the new workspace to Cargo.toml
-sed -E -i "s/(^    \".+\"$)/\1,\n    \"$FOLDER\"/g" Cargo.toml
+new_rust() {
+    # Append the new workspace to Cargo.toml
+    sed -E -i "s/(^    \".+\"$)/\1,\n    \"$FOLDER\"/g" Cargo.toml
 
-cargo new $FOLDER
-cd $FOLDER
-cargo add aoc_utils
+    cargo new $FOLDER
+    cd $FOLDER
+    cargo add aoc_utils
 
-cat > src/main.rs << EOF
+    cat > src/main.rs << EOF
 use std::fs;
 use aoc_utils::submit;
 
@@ -30,10 +31,59 @@ fn main() {
     // submit("1", false);
 }
 EOF
+}
 
-if [ -e "input" ]; then
-    echo 'input file already exists' >&2
-    exit 1
-fi
+new_python() {
+    mkdir $FOLDER
+    cd $FOLDER
+    cat > main.py << EOF
+import aoc_utils
+from typing import List
 
-curl -O --cookie $COOKIES "https://adventofcode.com/2023/day/$DAY/input"
+
+PATH = "input"
+# PATH = "example"
+
+
+def part1(lines: List[str]) -> str:
+    pass
+
+
+def part2(lines: List[str]) -> str:
+    pass
+
+
+def submit(answer: str, part2: bool) -> None:
+    part = "2" if part2 else "1"
+    i = input(f"Press 'y' to submit '{answer}' for part {part}?: ")
+
+    if i == "y":
+        print("Submitting...")
+        aoc_utils.submit(answer, part2)
+
+
+def main() -> None:
+    with open(PATH) as f:
+        lines = f.readlines()
+        submit(str(part1(lines)), False)
+        # submit(str(part2(lines)), True)
+
+
+if __name__ == "__main__":
+    main()
+EOF
+}
+
+main() {
+    # new_rust
+    new_python
+
+    if [ -e "input" ]; then
+        echo 'input file already exists' >&2
+        exit 1
+    fi
+
+    curl -O --cookie $COOKIES "https://adventofcode.com/2023/day/$DAY/input"
+}
+
+main
