d0309aade440178811d42c8067d3cf05ed0b00d6
kugiyasan <kugiyasan@users.noreply.github.com>
2023-12-03 14:45:57 -0500

    day 3 part 2

--- d3_gear_ratios/main.py
+++ d3_gear_ratios/main.py
@@ -9,7 +9,7 @@ PATH = "input"
 Vec2 = Tuple[int, int]
 
 
-def find_symbol(lines: List[str], top_left: Vec2, bottom_right: Vec2) -> bool:
+def find_symbol(lines: List[str], top_left: Vec2, bottom_right: Vec2) -> Vec2:
     for y in range(top_left[1], bottom_right[1] + 1):
         if y < 0 or len(lines) <= y:
             print("skipping y")
@@ -23,27 +23,32 @@ def find_symbol(lines: List[str], top_left: Vec2, bottom_right: Vec2) -> bool:
             c = lines[y][x]
             print(f"{c}", end="")
 
-            if not c.isdigit() and c != ".":
+            # if not c.isdigit() and c != ".":
+            if c == "*":
                 print()
-                return True
+                return x, y
 
         print()
 
     n = lines[top_left[1] + 1][top_left[0]+1:bottom_right[0]-1]
     print(f"{top_left=} {bottom_right=} {n}")
-    return False
 
 
 def part1(lines: List[str]) -> str:
-    # lines = lines[102:105]
     s = 0
+    near_gear = {}
 
     for i, line in enumerate(lines):
         for m in re.finditer(r"\d+", line):
             # print(m)
 
-            if find_symbol(lines, (m.start() - 1, i - 1), (m.end() + 1, i + 1)):
-                s += int(line[m.start():m.end()])
+            coord = find_symbol(lines, (m.start() - 1, i - 1), (m.end() + 1, i + 1))
+            if coord is not None:
+                n = int(line[m.start():m.end()])
+                if near_gear.get(coord) is not None:
+                    s += near_gear[coord] * n
+                else:
+                    near_gear[coord] = n
 
     print(s)
     return str(s)
@@ -68,7 +73,7 @@ def submit(answer: str, part2: bool) -> None:
 def main() -> None:
     with open(PATH) as f:
         lines = [s.strip() for s in f.readlines()]
-        submit(str(part1(lines)), False)
+        submit(str(part1(lines)), True)
         # submit(str(part2(lines)), True)
 
 
