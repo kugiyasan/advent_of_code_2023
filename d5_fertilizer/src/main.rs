d2ede24d16d30047df08b9d5e62f89356f6aead7
kugiyasan <kugiyasan@users.noreply.github.com>
2023-12-05 01:03:46 -0500

    day 5 part 2

--- d5_fertilizer/src/main.rs
+++ d5_fertilizer/src/main.rs
@@ -1,5 +1,7 @@
 use aoc_utils::parse_ints;
 use aoc_utils::submit;
+use rayon::prelude::IntoParallelRefMutIterator;
+use rayon::prelude::ParallelIterator;
 use std::fs;
 
 fn get_value_from_map(seed: i64, m: &Vec<Vec<i64>>) -> i64 {
@@ -30,22 +32,28 @@ fn main() {
         .map(|m| m.split('\n').map(parse_ints).collect::<Vec<_>>());
 
     let seeds = &mut input.next().unwrap()[0];
-    // let seeds = &mut seeds
-    //     .chunks(2)
-    //     .flat_map(|r| (r[0]..r[0] + r[1]).collect::<Vec<_>>())
-    //     .collect::<Vec<_>>();
+
+    // comment for part 1
+    let mut seeds = seeds
+        .chunks(2)
+        .flat_map(|r| (r[0]..r[0] + r[1]).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
 
     println!("len {}", seeds.len());
 
     let maps: Vec<Vec<_>> = input.map(|m| m.into_iter().skip(1).collect()).collect();
 
-    for seed in seeds.iter_mut() {
-        for m in &maps {
-            *seed = get_value_from_map(*seed, m);
-        }
-    }
+    let min = seeds
+        .par_iter_mut()
+        .map(|seed| {
+            let mut s = *seed;
+            for m in &maps {
+                s = get_value_from_map(s, m);
+            }
+            s
+        })
+        .min()
+        .unwrap();
 
-    let min = seeds.iter().min().unwrap();
-    // println!("{:?} min {}", seeds, min);
     submit(&min.to_string(), part2);
 }
