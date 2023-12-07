d526c88e682f7b24130992c4467d983c2cad00c7
kugiyasan <kugiyasan@users.noreply.github.com>
2023-12-07 01:27:31 -0500

    day 7 part 1 cleanup

--- d7_camel_cards/src/main.rs
+++ d7_camel_cards/src/main.rs
@@ -3,13 +3,6 @@ use std::fs;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum HandType {
-    // FiveOfAKind([usize; 1]),
-    // FourOfAKind([usize; 2]),
-    // FullHouse([usize; 2]),
-    // ThreeOfAKind([usize; 3]),
-    // TwoPairs([usize; 3]),
-    // OnePair([usize; 4]),
-    // HighCard([usize; 5]),
     FiveOfAKind,
     FourOfAKind,
     FullHouse,
@@ -27,7 +20,6 @@ struct Hand {
 
 fn parse_hand(hand: &str) -> Hand {
     let cards = b"AKQJT98765432";
-    // let cards = b"23456789TJQKA";
 
     let hand = hand.bytes().collect::<Vec<_>>();
 
@@ -41,70 +33,30 @@ fn parse_hand(hand: &str) -> Hand {
         .map(|h| cards.iter().position(|c| c == h).unwrap())
         .collect::<Vec<_>>();
 
-    let two_index = counts.iter().position(|&count| count == 2);
-    let three_index = counts.iter().position(|&count| count == 3);
-    let four_index = counts.iter().position(|&count| count == 4);
-    let five_index = counts.iter().position(|&count| count == 5);
-
-    let pairs = counts
+    let pairs_count = counts
         .iter()
         .filter(|&&count| count == 2)
-        .collect::<Vec<_>>();
-    let singles = counts.iter().filter(|&&c| c == 1).collect::<Vec<_>>();
-
-    if let Some(i) = two_index {
-        if let Some(j) = three_index {
-            // return Hand::FullHouse([i, j]);
-            return Hand {
-                kind: HandType::FullHouse,
-                bytes: hand,
-            };
-        }
-    }
+        .count();
 
     let max_of_a_kind = *counts.iter().max().unwrap();
-    println!("{}", max_of_a_kind);
 
-    if max_of_a_kind == 5 {
-        // return Hand::FiveOfAKind([five_index.unwrap()]);
+    if counts.contains(&2) && counts.contains(&3) {
         return Hand {
-            kind: HandType::FiveOfAKind,
-            bytes: hand,
-        };
-    } else if max_of_a_kind == 4 {
-        // return Hand::FourOfAKind([four_index.unwrap(), *singles[0]]);
-        return Hand {
-            kind: HandType::FourOfAKind,
-            bytes: hand,
-        };
-    } else if max_of_a_kind == 3 {
-        // return Hand::ThreeOfAKind([three_index.unwrap(), *singles[0], *singles[1]]);
-        return Hand {
-            kind: HandType::ThreeOfAKind,
+            kind: HandType::FullHouse,
             bytes: hand,
         };
     }
 
-    if pairs.len() == 2 {
-        // return Hand::TwoPairs([*pairs[0], *pairs[1], *singles[0]]);
-        return Hand {
-            kind: HandType::TwoPairs,
-            bytes: hand,
-        };
-    } else if pairs.len() == 1 {
-        // return Hand::OnePair([*pairs[0], *singles[0], *singles[1], *singles[2]]);
-        return Hand {
-            kind: HandType::OnePair,
-            bytes: hand,
-        };
-    }
-
-    // Hand::HighCard([0; 5])
-
-    return Hand {
-        kind: HandType::HighCard,
-        bytes: hand,
+    let kind = match (max_of_a_kind, pairs_count) {
+        (5, _) => HandType::FiveOfAKind,
+        (4, _) => HandType::FourOfAKind,
+        (3, _) => HandType::ThreeOfAKind,
+        (_, 2) => HandType::TwoPairs,
+        (_, 1) => HandType::OnePair,
+        _ => HandType::HighCard,
     };
+
+    Hand { kind, bytes: hand }
 }
 
 fn main() {
@@ -124,7 +76,7 @@ fn main() {
     let s = input
         .iter()
         .enumerate()
-        .map(|(i, (hand, bid))| (i as i32 + 1) * bid)
+        .map(|(i, (_hand, bid))| (i as i32 + 1) * bid)
         .collect::<Vec<_>>();
     let sum: i32 = s.iter().sum();
 
